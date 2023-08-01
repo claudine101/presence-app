382f6ccb8f59c95e6a7a886ee9a05bc9c0507bf3
vanny <vdevelopper@gmail.com>
2023-08-01 19:16:46 +0300

    feat(elements):finalisation d'affichage de volumes

--- src/screens/scanning/chefEquipe/AllVolumesRecusScreen.jsx
+++ src/screens/scanning/chefEquipe/AllVolumesRecusScreen.jsx
@@ -1,9 +1,11 @@
-import React from "react";
-import { FlatList, StyleSheet, Text, View, TouchableNativeFeedback } from "react-native";
+import React, { useCallback, useState } from "react";
+import { FlatList, StyleSheet, Text, View, TouchableNativeFeedback, ActivityIndicator, Image } from "react-native";
 import AppHeader from "../../../components/app/AppHeader";
 import { COLORS } from "../../../styles/COLORS";
 import { AntDesign } from '@expo/vector-icons';
-import { useNavigation } from "@react-navigation/native";
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
+import fetchApi from "../../../helpers/fetchApi";
+import moment from 'moment'
 
 
 /**
@@ -15,30 +17,75 @@ import { useNavigation } from "@react-navigation/native";
 
 export default function AllVolumesRecusScreen() {
         const navigation = useNavigation()
+        const [allVolumes, setAllVolumes] = useState([])
+        const [loading, setLoading] = useState(false)
+
+        //fonction pour recuperer les volumes associer a un chef d'equipe
+        useFocusEffect(useCallback(() => {
+                (async () => {
+                        try {
+                                setLoading(true)
+                                const vol = await fetchApi(`/scanning/volume/sup/aille`)
+                                setAllVolumes(vol.result)
+                        } catch (error) {
+                                console.log(error)
+                        } finally {
+                                setLoading(false)
+                        }
+                })()
+        }, []))
+
         return (
                 <>
                         <AppHeader />
                         <View style={styles.container}>
-                                <TouchableNativeFeedback useForeground background={TouchableNativeFeedback.Ripple(COLORS.handleColor)} 
-                                        onPress={()=>navigation.navigate("NewChefPlateauScreen")}
-                                >
-                                        <View style={styles.cardDetails}>
-                                                <View style={styles.carddetailItem}>
-                                                        <View style={styles.cardImages}>
-                                                                <AntDesign name="folderopen" size={24} color="black" />
-                                                        </View>
-                                                        <View style={styles.cardDescription}>
-                                                                <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
-                                                                        <View>
-                                                                                <Text style={styles.itemVolume}>eehhdehdh</Text>
-                                                                                {/* <Text>{volume.CODE_VOLUME}</Text> */}
-                                                                        </View>
-                                                                        <Text>12/12/2023</Text>
-                                                                </View>
-                                                        </View>
-                                                </View>
-                                        </View>
-                                </TouchableNativeFeedback>
+                                {loading ? <View style={{ flex: 1, alignContent: 'center', alignItems: 'center', justifyContent: 'center' }}>
+                                        <ActivityIndicator animating size={'large'} color={'#777'} />
+                                </View>:
+                                //  <View style={styles.emptyContaier}>
+                                //         <Image source={require('../../../../assets/images/mail-receive.png')} style={styles.emptyImage} />
+                                //         <Text style={styles.emptyTitle}>
+                                //                 Aucun volume trouvé
+                                //         </Text>
+                                //         <Text style={styles.emptyDesc}>
+                                //                 Aucun volume planifier ou vous n'êtes pas affecte a aucun volume
+                                //         </Text>
+                                // </View>
+                                <FlatList
+                                        style={styles.contain}
+                                        data={allVolumes}
+                                        renderItem={({ item: volume, index }) => {
+                                                return (
+                                                        <>
+                                                                {loading ? <View style={{ flex: 1, alignContent: 'center', alignItems: 'center', justifyContent: 'center' }}>
+                                                                        <ActivityIndicator animating size={'large'} color={'#777'} />
+                                                                </View> :
+                                                                        <TouchableNativeFeedback useForeground background={TouchableNativeFeedback.Ripple(COLORS.handleColor)}
+                                                                                onPress={() => navigation.navigate("NewChefPlateauScreen")}
+                                                                        >
+                                                                                <View style={styles.cardDetails}>
+                                                                                        <View style={styles.carddetailItem}>
+                                                                                                <View style={styles.cardImages}>
+                                                                                                        <AntDesign name="folderopen" size={24} color="black" />
+                                                                                                </View>
+                                                                                                <View style={styles.cardDescription}>
+                                                                                                        <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+                                                                                                                <View>
+                                                                                                                        <Text style={styles.itemVolume}>{volume.NOM} {volume.PRENOM}</Text>
+                                                                                                                        {/* <Text>{volume.CODE_VOLUME}</Text> */}
+                                                                                                                </View>
+                                                                                                                <Text>12/12/2023</Text>
+                                                                                                        </View>
+                                                                                                </View>
+                                                                                        </View>
+                                                                                </View>
+                                                                        </TouchableNativeFeedback>
+                                                                }
+                                                        </>
+                                                )
+                                        }}
+                                        keyExtractor={(volume, index) => index.toString()}
+                                />}
                         </View>
                 </>
         )
@@ -78,5 +125,27 @@ const styles = StyleSheet.create({
         itemVolume: {
                 fontSize: 15,
                 fontWeight: "bold",
+        },
+        emptyContaier: {
+                justifyContent: 'center',
+                alignItems: 'center'
+        },
+        emptyImage: {
+                width: 100,
+                height: 100,
+                resizeMode: 'contain'
+        },
+        emptyTitle: {
+                textAlign: 'center',
+                fontWeight: 'bold',
+                color: '#333',
+                marginVertical: 10,
+                fontSize: 15
+        },
+        emptyDesc: {
+                color: '#777',
+                textAlign: 'center',
+                maxWidth: 300,
+                lineHeight: 20
         },
 })
