7195fffdafe69ee185e801257f5586d394ce1328
claudine101 <ndayisabaclaudine01@gmail.com>
2023-09-01 23:01:57 +0300

    correction

--- src/screens/indexation/chefPlateau/SelectAgentIndexationScreen.jsx
+++ src/screens/indexation/chefPlateau/SelectAgentIndexationScreen.jsx
@@ -76,7 +76,6 @@ export default function SelectAgentIndexationScreen() {
                     agentsModalRef.current?.close()
                     handleChange('agent', agent)
           }
-
           useFocusEffect(useCallback(() => {
                     (async () => {
                               try {
